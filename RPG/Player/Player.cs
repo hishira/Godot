83014f660ce1fb1b305b167e27e71ca902c44305
hishira <okami0124@gmail.com>
2023-01-06 18:41:02 +0100

    Update animation

--- RPG/Player/Player.cs
+++ RPG/Player/Player.cs
@@ -3,38 +3,36 @@ using System;
 
 public class Player : KinematicBody2D, InputHandle
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+
     public const int ACCELERATION = 400;
     public const int FREACTION = 80;
     public const int MAX_SPEED = 400;
     private Vector2 velocity = Vector2.Zero;
+    private AnimationPlayer animation = null;
+    private AnimationTree animationTree = null;
+    private AnimationNodeStateMachinePlayback animationState = null;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("Start up");
+        this.animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
+        this.animationTree = this.GetNode("AnimationTree") as AnimationTree;
+        // Get access to animation state
+        this.animationState = this.animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
     }
 
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        //this.delta(delta);
         this.inputHandle(delta);
-    }
-
-    private void delta(float delta)
-    {
-        GD.Print(delta);
     }
 
     public void inputHandle(float delta)
     {
         Vector2 inputVector = this.prepareInputVector();
         this.updateVelocity(delta, inputVector);
-        //GD.Print("Velocity, ", this.velocity * delta);
-        if (this.velocity != Vector2.Zero){
+        if (this.velocity != Vector2.Zero)
+        {
             this.MoveAndSlide(this.velocity);
         }
     }
@@ -45,19 +43,32 @@ public class Player : KinematicBody2D, InputHandle
         inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
         inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
         inputVector = inputVector.Normalized();
-        //GD.Print("Input vector, ", inputVector);
         return inputVector;
     }
 
+    private void handleAnimationChange(Vector2 inputVector, string value)
+    {
+        if (inputVector != Vector2.Zero)
+        {
+            this.animationTree.Set("parameters/Idle/blend_position", inputVector);
+            this.animationTree.Set("parameters/Run/blend_position", inputVector);
+        }
+        this.animationState.Travel(value);
+
+    }
     private void updateVelocity(float delta, Vector2 inputVector)
     {
         if (inputVector != Vector2.Zero)
         {
+
+            this.handleAnimationChange(inputVector, "Run");
             this.velocity = this.velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
         }
         else
         {
+            this.handleAnimationChange(inputVector, "Idle");
             this.velocity = this.velocity.MoveToward(Vector2.Zero, FREACTION * delta);
         }
+
     }
 }
