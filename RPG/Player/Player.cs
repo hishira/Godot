3d5173ce329ef42853f99da84ea8188472955481
hishira <okami0124@gmail.com>
2023-01-06 20:14:33 +0100

    Update and refactor Player script

--- RPG/Player/Player.cs
+++ RPG/Player/Player.cs
@@ -3,70 +3,75 @@ using Godot;
 public class Player : KinematicBody2D, InputHandle
 {
 
-	public const int ACCELERATION = 400;
-	public const int FREACTION = 80;
-	public const int MAX_SPEED = 400;
-	private Vector2 velocity = Vector2.Zero;
-	private AnimationPlayer animation = null;
-	private AnimationTree animationTree = null;
-	private AnimationNodeStateMachinePlayback animationState = null;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		this.animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
-		this.animationTree = this.GetNode("AnimationTree") as AnimationTree;
-		// Get access to animation state
-		this.animationState = this.animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
-	}
+    public const int ACCELERATION = 500;
+    public const int FREACTION = 500;
+    public const int MAX_SPEED = 250;
+
+    public PlayerStruct playerInfo;
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        AnimationPlayer animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
+        AnimationTree animationTree = this.GetNode("AnimationTree") as AnimationTree;
+        // Get access to animation state
+        AnimationNodeStateMachinePlayback animationState = animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
+        this.playerInfo = new PlayerStruct(Vector2.Zero, animation, animationTree, animationState);
+    }
 
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(float delta)
-	{
-		this.inputHandle(delta);
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        this.inputHandle(delta);
+    }
 
-	public void inputHandle(float delta)
-	{
-		Vector2 inputVector = this.prepareInputVector();
-		this.updateVelocity(delta, inputVector);
-		if (this.velocity != Vector2.Zero)
-		{
-			this.MoveAndSlide(this.velocity);
-		}
-	}
+    public void inputHandle(float delta)
+    {
+        Vector2 inputVector = this.prepareInputVector();
+        this.updateVelocity(delta, inputVector);
+        if (this.playerInfo.isNotZero())
+        {
+            this.MoveAndSlide(this.playerInfo.velocity);
+        }
+    }
 
-	private Vector2 prepareInputVector()
-	{
-		Vector2 inputVector = Vector2.Zero;
-		inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-		inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-		inputVector = inputVector.Normalized();
-		return inputVector;
-	}
+    private Vector2 prepareInputVector()
+    {
+        Vector2 inputVector = Vector2.Zero;
+        inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        inputVector = inputVector.Normalized();
+        return inputVector;
+    }
 
-	private void handleAnimationChange(Vector2 inputVector, string value)
-	{
-		if (inputVector != Vector2.Zero)
-		{
-			this.animationTree.Set("parameters/Idle/blend_position", inputVector);
-			this.animationTree.Set("parameters/Run/blend_position", inputVector);
-		}
-		this.animationState.Travel(value);
+    private void handleAnimationChange(Vector2 inputVector, string value)
+    {
+        if (inputVector != Vector2.Zero)
+        {
+            this.playerInfo.animationTree.Set("parameters/Idle/blend_position", inputVector);
+            this.playerInfo.animationTree.Set("parameters/Run/blend_position", inputVector);
+        }
+        this.playerInfo.animationState.Travel(value);
 
-	}
-	private void updateVelocity(float delta, Vector2 inputVector)
-	{
-		if (inputVector != Vector2.Zero)
-		{
-			this.handleAnimationChange(inputVector, "Run");
-			this.velocity = this.velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
-		}
-		else
-		{
-			this.handleAnimationChange(inputVector, "Idle");
-			this.velocity = this.velocity.MoveToward(Vector2.Zero, FREACTION * delta);
-		}
+    }
+    private void updateVelocity(float delta, Vector2 inputVector)
+    {
+        if (inputVector != Vector2.Zero)
+        {
+            this.handleAnimationChange(inputVector, "Run");
+            this.playerInfo.velocity = this.moveVelocityVector(inputVector * MAX_SPEED, ACCELERATION * delta);
+        }
+        else
+        {
+            this.handleAnimationChange(inputVector, "Idle");
+            this.playerInfo.velocity = this.moveVelocityVector(Vector2.Zero, FREACTION * delta);
+        }
 
-	}
+    }
+
+    private Vector2 moveVelocityVector(Vector2 input, float delta)
+    {
+        return this.playerInfo.velocity.MoveToward(input, delta);
+    }
 }
