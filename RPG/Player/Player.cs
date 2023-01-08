2722eba5afb0f446474e3191f273a7d13d535aba
hishira <okami0124@gmail.com>
2023-01-08 15:53:49 +0100

    Create attack animation and state

--- RPG/Player/Player.cs
+++ RPG/Player/Player.cs
@@ -1,10 +1,10 @@
 using Godot;
 
-public class Player : KinematicBody2D, InputHandle
+public class Player : KinematicBody2D
 {
 
     public PlayerInfo playerInfo;
-    
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -13,30 +13,49 @@ public class Player : KinematicBody2D, InputHandle
         // Get access to animation state
         AnimationNodeStateMachinePlayback animationState = animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
         this.playerInfo = new PlayerInfo(Vector2.Zero, animation, animationTree, animationState);
+        this.playerInfo.setAnimation(true);
     }
 
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        this.inputHandle(delta);
+        switch (this.playerInfo.playerState)
+        {
+            case PlayerState.Move:
+                {
+                    this.moveHandle(delta);
+                    break;
+                }
+            case PlayerState.Attack:
+                {
+                    this.attackHandle(delta);
+                    break;
+                }
+        }
     }
 
+    private void moveHandle(float delta)
+    {
+        this.inputHandle(delta);
+    }
     public void inputHandle(float delta)
     {
-        this.playerInfo.updateVelocity(delta, this.prepareInputVector());
+        this.playerInfo.updateVelocity(delta, this.playerInfo.prepareInputVector());
         if (this.playerInfo.isNotZero())
         {
             this.MoveAndSlide(this.playerInfo.velocity);
         }
     }
 
-    private Vector2 prepareInputVector()
+    public void attackHandle(float delta)
     {
-        Vector2 inputVector = Vector2.Zero;
-        inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-        inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-        inputVector = inputVector.Normalized();
-        return inputVector;
+        this.playerInfo.attackHandle(delta);
+        this.MoveAndSlide(this.playerInfo.velocity);
     }
+
+    public void attackAnimationEnd(){
+        this.playerInfo.changeState(PlayerState.Move);
+    }
+
 }
--- RPG/Player/PlayerInfo.cs
+++ RPG/Player/PlayerInfo.cs
@@ -1,16 +1,23 @@
 using Godot;
 readonly public struct PlayerStruct
 {
-    public int ACCELERATION{get;}
-    public int FREACTION{get;}
-    public int MAX_SPEED{get;}
+    public int ACCELERATION { get; }
+    public int FREACTION { get; }
+    public int MAX_SPEED { get; }
 
-    public PlayerStruct(int acceleration = 500, int freaction = 500, int max_spped = 250) {
+    public PlayerStruct(int acceleration = 500, int freaction = 500, int max_spped = 250)
+    {
         this.ACCELERATION = acceleration;
         this.FREACTION = freaction;
         this.MAX_SPEED = max_spped;
     }
 
+}
+public enum PlayerState
+{
+    Move,
+    Roll,
+    Attack,
 }
 
 public class PlayerInfo
@@ -21,6 +28,8 @@ public class PlayerInfo
     public AnimationPlayer animation;
     public AnimationTree animationTree;
     public AnimationNodeStateMachinePlayback animationState;
+    
+    public PlayerState playerState;
 
     public PlayerInfo(Vector2 velocity, AnimationPlayer animation, AnimationTree animationTree, AnimationNodeStateMachinePlayback animationState)
     {
@@ -29,6 +38,12 @@ public class PlayerInfo
         this.animationTree = animationTree;
         this.animationState = animationState;
         this.playerStat = new PlayerStruct(500, 500, 250);
+        this.playerState = PlayerState.Move;
+    }
+
+    public void setAnimation(bool value)
+    {
+        this.animationTree.Active = value;
     }
 
     public bool isNotZero()
@@ -50,6 +65,29 @@ public class PlayerInfo
         }
     }
 
+    public Vector2 prepareInputVector()
+    {
+        Vector2 inputVector = Vector2.Zero;
+        inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        inputVector = inputVector.Normalized();
+        if(Input.IsActionJustPressed("attack")){
+            this.playerState = PlayerState.Attack;
+            GD.Print("Attack");
+        } else {
+            this.playerState = PlayerState.Move;
+        }
+        return inputVector;
+    }
+
+    public void attackHandle(float delta){
+        this.velocity = this.moveVelocityVector(this.velocity / 4, this.playerStat.FREACTION * delta);
+        this.animationState.Travel("Attack");
+    }
+
+    public void changeState(PlayerState state){
+        this.playerState = state;
+    }
     private Vector2 moveVelocityVector(Vector2 input, float delta)
     {
         return this.velocity.MoveToward(input, delta);
@@ -61,8 +99,12 @@ public class PlayerInfo
         {
             this.animationTree.Set("parameters/Idle/blend_position", inputVector);
             this.animationTree.Set("parameters/Run/blend_position", inputVector);
+            this.animationTree.Set("parameters/Attack/blend_position", inputVector);
         }
         this.animationState.Travel(value);
 
     }
+
+
+
 }
