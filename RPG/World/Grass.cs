fb187e5ddbd971088183131196d2bdd0b8c16c40
hishira <okami0124@gmail.com>
2023-01-09 17:54:39 +0100

    Move effect to grass

--- RPG/World/Grass.cs
+++ RPG/World/Grass.cs
@@ -9,6 +9,8 @@ public class Grass : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        AnimatedSprite asprite = this.GetNode("AnimatedSprite") as AnimatedSprite;
+        asprite.Visible = false;
 
     }
 
@@ -16,12 +18,26 @@ public class Grass : Node2D
     public override void _Process(float delta)
     {
         if(Input.IsActionJustPressed("attack")){
-            var GrassEffect = GD.Load("res://Effects/GrassEffect.tscn") as PackedScene;
-            var grassEffectInstance = GrassEffect.Instance() as Node2D;
-            var world = GetTree().CurrentScene;
-            grassEffectInstance.GlobalPosition = this.GlobalPosition;
-            world.AddChild(grassEffectInstance);
-            this.QueueFree();
+            //this.addGrassEffect();
+            AnimatedSprite asprite = this.GetNode("AnimatedSprite") as AnimatedSprite;
+            Sprite sprite = this.GetNode("Sprite") as Sprite;
+            sprite.Hide();
+            asprite.Visible = true;
+            asprite.Play("Animate");
         }
+    }
+
+    private void addGrassEffect(){
+        PackedScene GrassEffect = GD.Load("res://Effects/GrassEffect.tscn") as PackedScene;
+        // Create sceene instance and add to main scene 'World'
+        Node2D grassEffectInstance = GrassEffect.Instance<Node2D>();
+        Node world = GetTree().CurrentScene;
+        grassEffectInstance.GlobalPosition = this.GlobalPosition;
+        world.AddChild(grassEffectInstance);
+        this.QueueFree(); 
+    }
+
+    public void _on_AnimatedSprite_animation_finished(){
+        this.QueueFree();
     }
 }
