481cd6ce94f82b6d3d4094a8dc1753a7137cf198
hishira <okami0124@gmail.com>
2023-01-18 18:13:32 +0100

    UI update

--- RPG/UI/HealthUI.cs
+++ RPG/UI/HealthUI.cs
@@ -8,21 +8,32 @@ public class HealthUI : Control
     private int _maxHealth = 4;
 
     public TextureRect HartUIFull;
+    public TextureRect HartUIEmpty;
     public int Health
     {
         get { return this._healt; }
         set
         {
+
             this._healt = Mathf.Clamp(value, 0, this.Maxhealth);
+            if (this.HartUIFull != null)
+            {
+                this.HartUIFull.RectSize = new Vector2(this._healt * 15, this.HartUIFull.RectSize.y);
+            }
         }
     }
-    
+
     public int Maxhealth
     {
         get { return this._maxHealth; }
         set
         {
             this._maxHealth = Math.Max(value, 1);
+            if (this.HartUIEmpty != null)
+            {
+                this.HartUIEmpty.RectSize = new Vector2(this._maxHealth * 15, this.HartUIEmpty.RectSize.y);
+            }
+
         }
     }
 
@@ -31,10 +42,13 @@ public class HealthUI : Control
         Stats value = this.GetNode<Stats>("/root/PlayerStats");
         this.Maxhealth = value.MaxHealth;
         this.Health = value.health;
-        value.Connect("healthChange", this, "handleHeartChanged");  
+        this.HartUIFull = this.GetNode<TextureRect>("HeartUIFull");
+        this.HartUIEmpty = this.GetNode<TextureRect>("HeartUIEmpty");
+        value.Connect("healthChange", this, "handleHeartChanged");
     }
 
-    public void handleHeartChanged(int value){
+    public void handleHeartChanged(int value)
+    {
         this.Health = value;
     }
 
