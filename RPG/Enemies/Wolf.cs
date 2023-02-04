c95c524bcb83b6b8cf37642dea7b6b76948c08f4
hishira <okami0124@gmail.com>
2023-02-04 16:52:59 +0100

    Animation and wolf movemen handle, problem with animation sprite visibility

---
+++ RPG/Enemies/Wolf.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public class Wolf : KinematicBody2D
+{
+    int ACCELERATION = 300;
+    int MAXSPEED = 50;
+
+    int FRICTION = 200;
+    AnimationPlayer wolfAnimationPlayer;
+
+    AnimationTree wolfAnimationTree;
+
+    Vector2 velocity = Vector2.Zero;
+    RandomNumberGenerator rnd;
+    AnimationNodeStateMachinePlayback animationMachine;
+    public override void _Ready()
+    {
+        this.rnd = new RandomNumberGenerator();
+        this.wolfAnimationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
+        this.wolfAnimationTree = this.GetNode<AnimationTree>("AnimationTree");
+        this.animationMachine = this.wolfAnimationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
+        this.wolfAnimationTree.Active = true;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        int x = this.rnd.RandiRange(-100, 100);
+        int y = this.rnd.RandiRange(-100, 100);
+        Vector2 direction = this.GlobalPosition.DirectionTo(new Vector2(x, y)).Normalized();
+        GD.Print(direction);
+        if (direction != Vector2.Zero)
+        {
+            this.wolfAnimationTree.Set("parameters/Iddle/blend_position", direction);
+            this.wolfAnimationTree.Set("parameters/Run/blend_position", direction);
+            this.animationMachine.Travel("Run");
+        }
+        else
+            this.animationMachine.Travel("Iddle");
+        this.velocity = this.velocity.MoveToward(this.MAXSPEED * direction, delta * this.ACCELERATION);
+        this.velocity = this.MoveAndSlide(this.velocity);
+    }
+}
