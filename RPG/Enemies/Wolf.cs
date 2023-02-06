332fd752c1d1b0cddf408160725fb92c531751cd
hishira <okami0124@gmail.com>
2023-02-06 18:46:56 +0100

    Try to repair wold animation

--- RPG/Enemies/Wolf.cs
+++ RPG/Enemies/Wolf.cs
@@ -11,6 +11,9 @@ public class Wolf : KinematicBody2D
 
     AnimationTree wolfAnimationTree;
 
+    Sprite wolfLeftWalkSprite;
+
+    Sprite wolfDownUpWalkSprite;
     Vector2 velocity = Vector2.Zero;
     RandomNumberGenerator rnd;
     AnimationNodeStateMachinePlayback animationMachine;
@@ -20,6 +23,9 @@ public class Wolf : KinematicBody2D
         this.wolfAnimationPlayer = this.GetNode<AnimationPlayer>("AnimationPlayer");
         this.wolfAnimationTree = this.GetNode<AnimationTree>("AnimationTree");
         this.animationMachine = this.wolfAnimationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
+        this.wolfLeftWalkSprite = this.GetNode<Sprite>("LeftRun");
+        this.wolfDownUpWalkSprite = this.GetNode<Sprite>("DownUp");
+        this.animationMachine.Connect("stateChanged", this, "handleStateChange");
         this.wolfAnimationTree.Active = true;
     }
 
@@ -28,16 +34,54 @@ public class Wolf : KinematicBody2D
         int x = this.rnd.RandiRange(-100, 100);
         int y = this.rnd.RandiRange(-100, 100);
         Vector2 direction = this.GlobalPosition.DirectionTo(new Vector2(x, y)).Normalized();
-        GD.Print(direction);
+        
         if (direction != Vector2.Zero)
         {
+            if (direction.x > 0)
+            {
+                this.wolfLeftWalkSprite.FlipH = true;
+            }
+            else
+            {
+                this.wolfLeftWalkSprite.FlipH = false;
+            }
+            if (direction.y > 0)
+            {
+                this.wolfDownUpWalkSprite.FlipV = true;
+            }
+            else
+            {
+                this.wolfDownUpWalkSprite.FlipV = false;
+            }
             this.wolfAnimationTree.Set("parameters/Iddle/blend_position", direction);
             this.wolfAnimationTree.Set("parameters/Run/blend_position", direction);
             this.animationMachine.Travel("Run");
         }
         else
             this.animationMachine.Travel("Iddle");
+        
+        if (this.wolfAnimationPlayer.CurrentAnimation == "RunDownNormal")
+        {
+            this.wolfLeftWalkSprite.Hide();
+            this.wolfDownUpWalkSprite.Show();
+        }
+        if (this.wolfAnimationPlayer.CurrentAnimation == "RunLeftNormal")
+        {
+            this.wolfLeftWalkSprite.Show();
+            this.wolfDownUpWalkSprite.Hide();
+        }
         this.velocity = this.velocity.MoveToward(this.MAXSPEED * direction, delta * this.ACCELERATION);
         this.velocity = this.MoveAndSlide(this.velocity);
+    }
+
+    public void _on_AnimationPlayer_animation_changed(string oldname, string newname){
+        GD.Print(oldname, newname);
+    }
+    public void _on_AnimationPlayer_animation_started(string name){
+        GD.Print(name);
+    }
+
+    public void handleStateChange(string name){
+        GD.Print(name);
     }
 }
