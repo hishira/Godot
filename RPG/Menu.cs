0a8f411908b973617e09b63c64dd21ae13af26d4
hishira <okami0124@gmail.com>
2023-02-13 18:26:13 +0100

    Create menu scene and attach script

---
+++ RPG/Menu.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public class Menu : Node2D
+{
+    TextureButton start;
+    TextureButton load;
+    TextureButton exit;
+
+    int stateModule = 1;
+    public override void _Ready()
+    {
+        this.start = this.GetNode<TextureButton>("CanvasLayer/Container/Start");
+        this.load = this.GetNode<TextureButton>("CanvasLayer/Container/Load");
+        this.exit = this.GetNode<TextureButton>("CanvasLayer/Container/Exit");
+        this.start.Pressed = true;
+
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (Input.IsActionJustPressed("ui_down"))
+        {
+            this.checkButtonState(this.stateModule % 4);
+        }
+        this.stateModule = this.stateModule > 3 ? 1 : ++this.stateModule;
+
+    }
+
+    private void checkButtonState(int prest)
+    {
+        GD.Print(prest);
+        if (prest == 1)
+        {
+            this.start.Pressed = true;
+            this.load.Pressed = false;
+            this.exit.Pressed = false;
+        }
+        if (prest == 2)
+        {
+            this.start.Pressed = false;
+            this.load.Pressed = true;
+            this.exit.Pressed = false;
+        }
+        if (prest == 3)
+        {
+            this.start.Pressed = false;
+            this.load.Pressed = false;
+            this.exit.Pressed = true;
+        }
+
+    }
+}
