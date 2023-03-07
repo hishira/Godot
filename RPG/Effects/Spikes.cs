9bb50d538236754b6d1f99d35da7bddf7efc7698
hishira <okami0124@gmail.com>
2023-03-07 20:13:25 +0100

    Add script to spikes

---
+++ RPG/Effects/Spikes.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Spikes : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    AnimationPlayer animation;
+    public override void _Ready()
+    {   
+        this.animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
+        this.animation.Play("Start");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
