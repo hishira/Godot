9c7de21cecd2baa9f32813450588e9fde13713b8
hishira <okami0124@gmail.com>
2023-03-09 18:14:29 +0100

    try to timeout animate

--- RPG/Effects/Spikes.cs
+++ RPG/Effects/Spikes.cs
@@ -9,15 +9,36 @@ public class Spikes : Node2D
 
     // Called when the node enters the scene tree for the first time.
     AnimationPlayer animation;
+    Timer animationResetTimer;
+    Timer animationPlayTimer;
     public override void _Ready()
-    {   
+    {
         this.animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
-        this.animation.Play("Start");
+        this.animationResetTimer = this.GetNode<Timer>("Timer");
+        this.animationPlayTimer = this.GetNode<Timer>("StartAnimation");
+        this.animationPlayTimer.Start(2);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+
+    public void animationEnd()
+    {
+        this.animation.Stop(false);
+        this.animationResetTimer.Start(2);
+    }
+
+    public void _on_Timer_timeout(){
+        this.animation.Play("Stop");
+        this.animationPlayTimer.Start(2);
+    }
+
+    public void _on_StartAnimation_timeout(){
+        this.animation.Play("Start");
+        this.animation.Stop(true);
+        this.animation.Play("Start");
+    }
 }
