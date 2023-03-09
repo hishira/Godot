0468cdeec89de136532d1f1a05cf0f98b7c468af
hishira <okami0124@gmail.com>
2023-03-09 20:15:38 +0100

    Fix spike animation

--- RPG/Effects/Spikes.cs
+++ RPG/Effects/Spikes.cs
@@ -9,14 +9,10 @@ public class Spikes : Node2D
 
     // Called when the node enters the scene tree for the first time.
     AnimationPlayer animation;
-    Timer animationResetTimer;
-    Timer animationPlayTimer;
     public override void _Ready()
     {
         this.animation = this.GetNode<AnimationPlayer>("AnimationPlayer");
-        this.animationResetTimer = this.GetNode<Timer>("Timer");
-        this.animationPlayTimer = this.GetNode<Timer>("StartAnimation");
-        this.animationPlayTimer.Start(2);
+        this.animation.Play("Start");
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,20 +21,6 @@ public class Spikes : Node2D
     //      
     //  }
 
-    public void animationEnd()
-    {
-        this.animation.Stop(false);
-        this.animationResetTimer.Start(2);
-    }
+   
 
-    public void _on_Timer_timeout(){
-        this.animation.Play("Stop");
-        this.animationPlayTimer.Start(2);
-    }
-
-    public void _on_StartAnimation_timeout(){
-        this.animation.Play("Start");
-        this.animation.Stop(true);
-        this.animation.Play("Start");
-    }
 }
