57155259aa82c72bdf9314a44784deef100ba2b3
hishira <okami0124@gmail.com>
2023-03-17 18:21:48 +0100

    Add stats to save button to handle save or load

--- RPG/UI/Save.cs
+++ RPG/UI/Save.cs
@@ -2,14 +2,15 @@ using Godot;
 
 public class Save : AbstractTextureButton
 {
+    Stats stats;
     public override void _Ready()
     {
-
+        this.stats = this.GetNode<Stats>("/root/Stats");
     }
 
     public override void clickHandle()
     {
-        GD.Print("Save");
+        GD.Print(this.stats.playerStats.LEVEL);
     }
 
 }
