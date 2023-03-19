b3e87acaf2854639e58b66158f2f7153cb400387
hishira <okami0124@gmail.com>
2023-03-19 18:39:16 +0100

    First try of implement save function

--- RPG/UI/Save.cs
+++ RPG/UI/Save.cs
@@ -1,11 +1,17 @@
 using Godot;
-
 public class Save : AbstractTextureButton
 {
     Stats stats;
     public override void _Ready()
     {
         this.stats = this.GetNode<Stats>("/root/Stats");
+        var saveGame = new File();
+        saveGame.Open("user://savegame.save", File.ModeFlags.Write);
+        var node = new Godot.Collections.Dictionary<string, uint>(){
+            { "Level", this.stats.playerStats.LEVEL}
+        };
+        saveGame.StoreLine(JSON.Print(node));
+        saveGame.Close();
     }
 
     public override void clickHandle()
